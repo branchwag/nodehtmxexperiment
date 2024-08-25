4930578702e129b4557e72a5927e17d98469735d
LeAnne B. <branchleannelmb@gmail.com>
2024-08-25 15:58:55 -0400

    fetches user from backend instead

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ app.use(express.urlencoded({ extended: true }));
 //parse json bodies as sent by api clients
 app.use(express.json());
 
+//handle GET - Users
+app.get("/users", (req, res) => {
+  const users = [
+    { id: 1, name: "Jimmy Dean" },
+    { id: 2, name: "Bertha Jones" },
+    { id: 3, name: "Rusty Slim" },
+  ];
+
+  res.send(`
+        <h1 class="text-2xl font-bond my-4">Users</h1>
+        <ul>
+        ${users.map((user) => `<li>${user.name}</li>`).join("")}</ul>
+        `);
+});
+
 //start server
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
