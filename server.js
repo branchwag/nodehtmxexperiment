cb8d94ebc3b726797a6355de30b78b6d2acda83c
LeAnne B. <branchleannelmb@gmail.com>
2024-08-25 16:05:34 -0400

    pulls users from external api

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //handle GET - Users
-app.get("/users", (req, res) => {
-  const users = [
-    { id: 1, name: "Jimmy Dean" },
-    { id: 2, name: "Bertha Jones" },
-    { id: 3, name: "Rusty Slim" },
-  ];
+app.get("/users", async (req, res) => {
+  //   const users = [
+  //     { id: 1, name: "Jimmy Dean" },
+  //     { id: 2, name: "Bertha Jones" },
+  //     { id: 3, name: "Rusty Slim" },
+  //   ];
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
 
   res.send(`
         <h1 class="text-2xl font-bond my-4">Users</h1>
